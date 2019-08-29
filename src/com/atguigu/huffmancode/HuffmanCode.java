31c3eb087698560ef57931804cb093d221e510a5
zhangyanglei <zhangyanglei@ucfgroup.com>
2019-08-29 18:53:15 +0800

    赫夫曼编码 done

---
+++ src/com/atguigu/huffmancode/HuffmanCode.java
@@ -0,0 +1,296 @@
+package com.atguigu.huffmancode;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HuffmanCode {
+
+    public static void main(String[] args) {
+//        String content = "i like like like java do you like a java";
+//        byte[] contentBytes = content.getBytes();
+//        byte[] huffmanZip = huffmanZip(contentBytes);
+//        System.out.println(Arrays.toString(huffmanZip));
+//        byte[] decode = decode(huffmanCodes, huffmanZip);
+//        System.out.println(new String(decode));
+//        String srcFile = "C:\\Users\\Administrator\\Desktop\\1565854573(1).jpg";
+//        String destFile = "C:\\Users\\Administrator\\Desktop\\dest.zip";
+//        zipFile(srcFile, destFile);
+        String zipFile = "C:\\Users\\Administrator\\Desktop\\dest.zip";
+        String destFile = "C:\\Users\\Administrator\\Desktop\\src2.jpg";
+        unZipFile(zipFile, destFile);
+    }
+
+    public static void unZipFile(String zipFile, String destFile) {
+        FileInputStream is = null;
+        ObjectInputStream ois = null;
+        FileOutputStream os = null;
+        try {
+            is = new FileInputStream(zipFile);
+            ois = new ObjectInputStream(is);
+            byte[] huffmanBytes = (byte[]) ois.readObject();
+            Map<Byte, String> huffmanCodes = (Map<Byte, String>) ois.readObject();
+            byte[] bytes = decode(huffmanCodes, huffmanBytes);
+            os = new FileOutputStream(destFile);
+            os.write(bytes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (os != null) {
+                    os.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (ois != null) {
+                    ois.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (is != null) {
+                    is.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public static void zipFile(String srcFile, String destFile) {
+        FileInputStream is = null;
+        FileOutputStream os = null;
+        ObjectOutputStream oos = null;
+        try {
+            is = new FileInputStream(srcFile);
+            byte[] b = new byte[is.available()];
+            is.read(b);
+            byte[] huffmanBytes = huffmanZip(b);
+            os = new FileOutputStream(destFile);
+            oos = new ObjectOutputStream(os);
+            oos.writeObject(huffmanBytes);
+            oos.writeObject(huffmanCodes);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (os != null) {
+                    os.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                if (oos != null) {
+                    oos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (is != null) {
+                    is.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private static byte[] decode(Map<Byte, String> huffmanCodes, byte[] huffmanBytes) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < huffmanBytes.length; i++) {
+            boolean flag = (i == huffmanBytes.length - 1);
+            stringBuilder.append(byteToBitString(!flag, huffmanBytes[i]));
+        }
+        Map<String, Byte> map = new HashMap<>();
+        huffmanCodes.forEach((k, v) -> {
+            map.put(v, k);
+        });
+        List<Byte> list = new ArrayList<>();
+        for (int i = 0; i < stringBuilder.length(); ) {
+            int count = 1;
+            boolean flag = true;
+            Byte b = null;
+            while (flag) {
+                String key = stringBuilder.substring(i, i + count);
+                b = map.get(key);
+                if (b != null) {
+                    flag = false;
+                } else {
+                    count++;
+                }
+            }
+            list.add(b);
+            i += count;
+        }
+        byte[] b = new byte[list.size()];
+        for (int i = 0; i < b.length; i++) {
+            b[i] = list.get(i);
+        }
+        return b;
+    }
+
+    private static String byteToBitString(boolean flag, byte b) {
+        int temp = b;
+        //如果是正数需要补高位
+        if (flag) {
+            temp |= 256;
+        }
+        //返回2进制补码
+        String str = Integer.toBinaryString(temp);
+        if (flag) {
+            return str.substring(str.length() - 8);
+        } else {
+            return str;
+        }
+    }
+
+    private static byte[] huffmanZip(byte[] bytes) {
+        List<Node> nodes = getNodes(bytes);
+        Node root = createHuffmanTree(nodes);
+        getCodes(root, "", stringBuilder);
+        return zip(bytes, huffmanCodes);
+    }
+
+
+    private static byte[] zip(byte[] bytes, Map<Byte, String> huffmanCodes) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (byte b : bytes) {
+            stringBuilder.append(huffmanCodes.get(b));
+        }
+        System.out.println(stringBuilder.toString());
+        int len;
+        if (stringBuilder.length() % 8 == 0) {
+            len = stringBuilder.length() / 8;
+        } else {
+            len = stringBuilder.length() / 8 + 1;
+        }
+        byte[] huffmanCodeBytes = new byte[len];
+        for (int i = 0; i < stringBuilder.length(); i += 8) {
+            String strByte;
+            if (i + 8 > stringBuilder.length()) {
+                strByte = stringBuilder.substring(i);
+            } else {
+                strByte = stringBuilder.substring(i, i + 8);
+            }
+            huffmanCodeBytes[i / 8] = (byte) Integer.parseInt(strByte, 2);
+        }
+        return huffmanCodeBytes;
+    }
+
+    static Map<Byte, String> huffmanCodes = new HashMap<>();
+    static StringBuilder stringBuilder = new StringBuilder();
+
+    private static Map<Byte, String> getCodes(Node root) {
+        if (root == null) {
+            return null;
+        }
+        getCodes(root.left, "0", stringBuilder);
+        getCodes(root.right, "1", stringBuilder);
+        return huffmanCodes;
+    }
+
+    private static void getCodes(Node node, String code, StringBuilder stringBuilder) {
+        StringBuilder stringBuilder2 = new StringBuilder(stringBuilder);
+        stringBuilder2.append(code);
+        if (node != null) {
+            if (node.data == null) {
+                getCodes(node.left, "0", stringBuilder2);
+                getCodes(node.right, "1", stringBuilder2);
+            } else {
+                huffmanCodes.put(node.data, stringBuilder2.toString());
+            }
+        }
+    }
+
+
+    private static void preOrder(Node root) {
+        if (root != null) {
+            root.preOrder();
+        } else {
+            System.out.println("赫夫曼树为空");
+        }
+    }
+
+    private static List<Node> getNodes(byte[] bytes) {
+        List<Node> nodes = new ArrayList<>();
+        Map<Byte, Integer> counts = new HashMap<>();
+        for (byte b : bytes) {
+            counts.merge(b, 1, Integer::sum);
+        }
+        for (Map.Entry<Byte, Integer> entry : counts.entrySet()) {
+            nodes.add(new Node(entry.getKey(), entry.getValue()));
+        }
+        return nodes;
+    }
+
+    private static Node createHuffmanTree(List<Node> nodes) {
+        while (nodes.size() > 1) {
+            Collections.sort(nodes);
+            Node leftNode = nodes.get(0);
+            Node rightNode = nodes.get(1);
+            Node parent = new Node(null, leftNode.weight + rightNode.weight);
+            parent.left = leftNode;
+            parent.right = rightNode;
+            nodes.remove(leftNode);
+            nodes.remove(rightNode);
+            nodes.add(parent);
+        }
+        return nodes.get(0);
+    }
+
+}
+
+class Node implements Comparable<Node> {
+
+    Byte data;
+    int weight;
+    Node left;
+    Node right;
+
+    public Node(Byte data, int weight) {
+        this.data = data;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        return this.weight - o.weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+            "data=" + data +
+            ", weight=" + weight +
+            '}';
+    }
+
+    public void preOrder() {
+        System.out.println(this);
+        if (this.left != null) {
+            this.left.preOrder();
+        }
+        if (this.right != null) {
+            this.right.preOrder();
+        }
+    }
+
+}
---
+++ src/com/atguigu/huffmantree/HuffmanTree.java
@@ -0,0 +1,79 @@
+package com.atguigu.huffmantree;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class HuffmanTree {
+
+    public static void main(String[] args) {
+        int[] arr = {13, 7, 8, 3, 29, 6, 1};
+        Node root = createHuffmanTree(arr);
+        preOrder(root);
+    }
+
+    public static void preOrder(Node root) {
+        if (root != null) {
+            root.preOrder();
+        } else {
+            System.out.println("是空树,不能遍历~~");
+        }
+
+    }
+
+    public static Node createHuffmanTree(int[] arr) {
+        List<Node> nodes = new ArrayList<>();
+        for (int value : arr) {
+            nodes.add(new Node(value));
+        }
+        while (nodes.size() > 1) {
+            Collections.sort(nodes);
+            Node leftNode = nodes.get(0);
+            Node rightNode = nodes.get(1);
+            Node parent = new Node(leftNode.value + rightNode.value);
+            parent.left = leftNode;
+            parent.right = rightNode;
+            nodes.remove(leftNode);
+            nodes.remove(rightNode);
+            nodes.add(parent);
+        }
+        System.out.println(nodes);
+        return nodes.get(0);
+    }
+}
+
+
+class Node implements Comparable<Node> {
+
+    int value;
+    Node left;
+    Node right;
+
+    public void preOrder() {
+        System.out.println(this);
+        if (this.left != null) {
+            this.left.preOrder();
+        }
+        if (this.right != null) {
+            this.right.preOrder();
+        }
+    }
+
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+            "value=" + value +
+            '}';
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        //从小到大进行排序
+        return this.value - o.value;
+    }
+}
