73773fc90fc8f669492da8f484fb9c7573842649
zhangyanglei <zhangyanglei@ucfgroup.com>
2019-09-03 17:26:43 +0800

    动态规划算法 kmp算法

---
+++ Algorithm/src/com/atguigu/kmp/KMPAlgorithm.java
@@ -0,0 +1,49 @@
+package com.atguigu.kmp;
+
+import java.util.Arrays;
+
+public class KMPAlgorithm {
+
+    public static void main(String[] args) {
+        String str1 = "BBC ABCDAB ABCDABCDABDE";
+        String str2 = "ABCDABD";
+        int[] next = kmpNext(str2);
+        System.out.println(Arrays.toString(next));
+        int index = kmpSearch(str1, str2, next);
+        System.out.println(index);
+
+    }
+
+    public static int kmpSearch(String str1, String str2, int[] next) {
+        for (int i = 0, j = 0; i < str1.length(); i++) {
+            while (j > 0 && str1.charAt(i) != str2.charAt(j)) {
+                j = next[j - 1];
+            }
+            if (str1.charAt(i) == str2.charAt(j)) {
+                j++;
+            }
+            if (j == str2.length()) {
+                return i - j + 1;
+            }
+        }
+        return -1;
+    }
+
+
+    public static int[] kmpNext(String dest) {
+        int[] next = new int[dest.length()];
+        next[0] = 0;
+        for (int i = 1, j = 0; i < dest.length(); i++) {
+            while (j > 0 && dest.charAt(i) != dest.charAt(j)) {
+                j = next[j - 1];
+            }
+            //满足时, 部分匹配值+1
+            if (dest.charAt(i) == dest.charAt(j)) {
+                j++;
+            }
+            next[i] = j;
+        }
+        return next;
+    }
+
+}
---
+++ Algorithm/src/com/atguigu/kmp/ViolenceMatch.java
@@ -0,0 +1,34 @@
+package com.atguigu.kmp;
+
+public class ViolenceMatch {
+
+    public static void main(String[] args) {
+        String str1 = "硅硅谷 尚硅谷你尚硅 尚硅谷你尚硅谷你尚硅你好";
+        String str2 = "尚硅谷你尚硅你";
+        int index = violenceMatch(str1, str2);
+        System.out.println(index);
+    }
+
+    public static int violenceMatch(String str1, String str2) {
+        char[] s1 = str1.toCharArray();
+        char[] s2 = str2.toCharArray();
+        int s1Len = s1.length;
+        int s2Len = s2.length;
+        int i = 0;
+        int j = 0;
+        while (i < s1Len && j < s2Len) {
+            if (s1[i] == s2[j]) {
+                i++;
+                j++;
+            } else {
+                i = i - (j - 1);
+                j = 0;
+            }
+        }
+        if (j == s2Len) {
+            return i - j;
+        } else {
+            return -1;
+        }
+    }
+}
