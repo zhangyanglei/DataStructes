1e9a6029f749b0b8455036bbda19e98e365998b1
zhangyanglei <zhangyanglei@ucfgroup.com>
2019-09-06 16:52:49 +0800

    骑士巡游

---
+++ Algorithm/src/com/atguigu/horse/HorseChessboard.java
@@ -0,0 +1,98 @@
+package com.atguigu.horse;
+
+import java.awt.Point;
+import java.util.ArrayList;
+
+public class HorseChessboard {
+
+
+    //棋盘的列
+    private static int X;
+    //棋盘的行
+    private static int Y;
+
+    //创建一个数组
+    private static boolean visited[];
+    private static boolean finished;
+
+    public static void main(String[] args) {
+        X = 6;
+        Y = 6;
+        int row = 1;
+        int column = 1;
+        int[][] chessboard = new int[X][Y];
+        visited = new boolean[X * Y];
+        long start = System.currentTimeMillis();
+        traversalChessboard(chessboard, row - 1, column - 1, 1);
+        long end = System.currentTimeMillis();
+        System.out.println("共耗时: " + (end - start) + " 毫秒");
+
+        for (int[] rows : chessboard) {
+            for (int step : rows) {
+                System.out.print(step + "\t");
+            }
+            System.out.println();
+        }
+
+    }
+
+    public static void traversalChessboard(int[][] chessboard, int row, int column, int step) {
+        chessboard[row][column] = step;
+        visited[row * X + column] = true;
+        ArrayList<Point> ps = next(new Point(column, row));
+        while (!ps.isEmpty()) {
+            Point p = ps.remove(0);
+            if (!visited[p.y * X + p.x]) {
+                traversalChessboard(chessboard, p.y, p.x, step + 1);
+            }
+        }
+        if (step < X * Y && !finished) {
+            chessboard[row][column] = 0;
+            visited[row * X + column] = false;
+        } else {
+            finished = true;
+        }
+    }
+
+
+    public static ArrayList<Point> next(Point curPoint) {
+        //创建一个ArrayList
+        ArrayList<Point> ps = new ArrayList<>();
+        //创建一个Point
+        Point p1 = new Point();
+        //表示马儿可以走5这个位置
+        if ((p1.x = curPoint.x - 2) >= 0 && (p1.y = curPoint.y - 1) >= 0) {
+            ps.add(new Point(p1));
+        }
+        //判断马儿可以走6这个位置
+        if ((p1.x = curPoint.x - 1) >= 0 && (p1.y = curPoint.y - 2) >= 0) {
+            ps.add(new Point(p1));
+        }
+        //判断马儿可以走7这个位置
+        if ((p1.x = curPoint.x + 1) < X && (p1.y = curPoint.y - 2) >= 0) {
+            ps.add(new Point(p1));
+        }
+        //判断马儿可以走0这个位置
+        if ((p1.x = curPoint.x + 2) < X && (p1.y = curPoint.y - 1) >= 0) {
+            ps.add(new Point(p1));
+        }
+        //判断马儿可以走1这个位置
+        if ((p1.x = curPoint.x + 2) < X && (p1.y = curPoint.y + 1) < Y) {
+            ps.add(new Point(p1));
+        }
+        //判断马儿可以走2这个位置
+        if ((p1.x = curPoint.x + 1) < X && (p1.y = curPoint.y + 2) < Y) {
+            ps.add(new Point(p1));
+        }
+        //判断马儿可以走3这个位置
+        if ((p1.x = curPoint.x - 1) >= 0 && (p1.y = curPoint.y + 2) < Y) {
+            ps.add(new Point(p1));
+        }
+        //判断马儿可以走4这个位置
+        if ((p1.x = curPoint.x - 2) >= 0 && (p1.y = curPoint.y + 1) < Y) {
+            ps.add(new Point(p1));
+        }
+        return ps;
+    }
+
+}
